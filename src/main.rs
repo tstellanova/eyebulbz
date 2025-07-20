c4192669af0a57dcab5ea5b209b7f11b2e49ce21
Todd Stellanova <tstellanova@users.noreply.github.com>
2025-07-19 17:30:04 -0700

    switch to lcd_async

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,29 @@
 #![no_std]
 #![no_main]
 
-use core::cell::RefCell;
 
 use defmt::*;
-use display_interface_spi::SPIInterface;
-use embassy_embedded_hal::shared_bus::blocking::spi::SpiDeviceWithConfig;
+use embassy_embedded_hal::shared_bus::asynch::spi::SpiDevice;
 use embassy_executor::Spawner;
 use embassy_rp::gpio::{Level, Output};
-use embassy_rp::spi;
+use embassy_rp::spi::{self, Async};
 use embassy_rp::spi::Spi;
-use embassy_sync::blocking_mutex::raw::NoopRawMutex;
-use embassy_sync::blocking_mutex::Mutex;
+use embassy_sync::{blocking_mutex::raw::NoopRawMutex, mutex::Mutex};
 use embassy_time::{Delay};
 
 use embedded_graphics::image::{Image};
 use embedded_graphics::pixelcolor::Rgb565;
 use embedded_graphics::prelude::*;
 
-// use lcd_async::models::ST7789;
-// use lcd_async::options::{Orientation, Rotation};
-// use lcd_async::Builder;
-// use lcd_async::options::ColorInversion;
+use static_cell::StaticCell;
 
-use mipidsi::models::ST7789;
-use mipidsi::options::{Orientation, Rotation};
-use mipidsi::Builder;
-use mipidsi::options::ColorInversion;
+use lcd_async::{
+    interface::SpiInterface,
+    models::ST7789,
+    options::{ColorInversion, Orientation, Rotation},
+    raw_framebuf::RawFrameBuf,
+    Builder,
+};
 
 use tinyqoi::Qoi;
 
@@ -35,8 +32,11 @@ use {defmt_rtt as _, panic_probe as _};
 
 const DISPLAY_FREQ: u32 = 64_000_000;
 
-const DISPLAY_WIDTH: i32 = 320;
-const DISPLAY_HEIGHT: i32 = 240;
+const DISPLAY_WIDTH: usize = 320;
+const DISPLAY_HEIGHT: usize = 240;
+const PIXEL_SIZE: usize = 2; // RGB565 = 2 bytes per pixel
+const FRAME_SIZE_BYTES: usize = DISPLAY_WIDTH * DISPLAY_HEIGHT  * PIXEL_SIZE;
+static FRAME_BUFFER: StaticCell<[u8; FRAME_SIZE_BYTES]> = StaticCell::new();
 
 
 #[embassy_executor::main]
@@ -59,9 +59,11 @@ async fn main(_spawner: Spawner) {
     display_config.polarity = spi::Polarity::IdleHigh;
 
     let spi = Spi::new(p.SPI1, clk, mosi, miso, p.DMA_CH0, p.DMA_CH1, display_config.clone());
-    let spi_bus: Mutex<NoopRawMutex, _> = Mutex::new(RefCell::new(spi));
-    let display_spi = 
-        SpiDeviceWithConfig::new(&spi_bus, Output::new(display_cs, Level::High), display_config);
+
+    // Create shared SPI bus
+    static SPI_BUS: StaticCell<Mutex<NoopRawMutex, Spi<'static, embassy_rp::peripherals::SPI1, Async>>> = StaticCell::new();
+    let spi_bus = SPI_BUS.init(Mutex::new(spi));
+    let spi_device = SpiDevice::new(spi_bus, Output::new(display_cs, Level::High));
 
     // dcx: 0 = command, 1 = data
     let dcx = Output::new(dcx, Level::Low);
@@ -71,26 +73,29 @@ async fn main(_spawner: Spawner) {
     let mut lcd_bl = Output::new(bl, Level::Low);
 
     // display interface abstraction from SPI and DC
-    let di = SPIInterface::new(display_spi, dcx);
+    let di = SpiInterface::new(spi_device, dcx);
 
     // Define the display from the display interface and initialize it
     let mut display = Builder::new(ST7789, di)
-        .display_size(240, 320)
         .reset_pin(rst)
+        .display_size(240, 320)
         .orientation(Orientation::new().rotate(Rotation::Deg90))
         .invert_colors(ColorInversion::Inverted)
-        // .with_framebuffer_size(DISPLAY_WIDTH as u16, DISPLAY_HEIGHT as u16)
         .init(&mut Delay)
+        .await
         .unwrap();
-    display.clear(Rgb565::BLACK).unwrap();
+
+    // Initialize frame buffer
+    let frame_buffer = FRAME_BUFFER.init([0; FRAME_SIZE_BYTES]);
 
     let img_data = include_bytes!("../img/240_dim_fleur.qoi");
 
     let qoi = Qoi::new(img_data).unwrap();
     let img_size = qoi.size();
-    let img_inset_point = Point::new(
-      (DISPLAY_WIDTH- img_size.width as i32)/2,
-      (DISPLAY_HEIGHT - img_size.height as i32)/2 );
+    let inset_x:i32 = (DISPLAY_WIDTH - img_size.width as usize).try_into().unwrap() ;
+    let inset_y:i32 = (DISPLAY_HEIGHT - img_size.height as usize).try_into().unwrap() ;
+    
+    let img_inset_point = Point::new(inset_x/2, inset_y/2);
     let img1 = Image::new(&qoi, img_inset_point);
 
     // create a three-frame animation sequence of image translations
@@ -111,12 +116,26 @@ async fn main(_spawner: Spawner) {
 
     loop {
         led.set_high();
-        // if img_idx == 0 {
-        //     display.clear(Rgb565::BLACK).unwrap();
-        // }
 
-        img_array[img_idx].draw(&mut display.color_converted()).unwrap(); 
+        // Create a framebuffer for drawing
+        let mut raw_fb =
+            RawFrameBuf::<Rgb565, _>::new(frame_buffer.as_mut_slice(), DISPLAY_WIDTH, DISPLAY_HEIGHT);
+
+        // Clear the framebuffer to black
+        raw_fb.clear(Rgb565::BLACK).unwrap();
+        
+        img_array[img_idx].draw(&mut raw_fb.color_converted()).unwrap(); 
         img_idx = (img_idx + 1) % 3;
+
+        lcd_bl.set_low();
+        // Send the framebuffer data to the display
+        display
+            .show_raw_data(0, 0, 
+                DISPLAY_WIDTH.try_into().unwrap(), DISPLAY_HEIGHT.try_into().unwrap(), 
+                frame_buffer)
+            .await
+            .unwrap();
+        lcd_bl.set_high();
 
         led.set_low();
     }
