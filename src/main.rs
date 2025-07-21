9780994d9d3519dca4808a378d93a489ae33e4ed
Todd Stellanova <tstellanova@users.noreply.github.com>
2025-07-21 13:23:52 -0700

    try using eg-transform

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,10 @@ use embedded_graphics::{
     primitives::{Circle, Primitive, PrimitiveStyle, Triangle, Ellipse},
 };
 
+use lcd_async::raw_framebuf;
 use static_cell::StaticCell;
+
+use embedded_graphics_transform::FlipX;
 
 use lcd_async::{
     interface::SpiInterface,
@@ -49,51 +52,51 @@ async fn main(_spawner: Spawner) {
     info!("Start Config");
 
     // LCD display 0: ST7789 pins
-    let rst0 = p.PIN_7; // SPI0 TX
     let bl0 = p.PIN_8; // SPI1 RX
+    let rst0 = p.PIN_7; // SPI0 TX
+    let dcx0 = p.PIN_6; // SPI0 SCK
+    let cs0 = p.PIN_5; // SPI0 CSN
     let miso0 = p.PIN_4; // SPI0 RX
     let mosi0 = p.PIN_3; // SPI0 TX
     let sck0 = p.PIN_2; // SPI0 SCK
-    let cs0 = p.PIN_5; // SPI0 CSN
-    let dcx0 = p.PIN_6; // SPI0 SCK
 
 
     // LCD display 1: ST7789 pins
     let rst1 = p.PIN_15; // SPI1 TX
+    let dcx1 = p.PIN_14; // SPI1 RX
     let bl1 = p.PIN_13; // SPI1 CSN
     let miso1 = p.PIN_12; // SPI1 RX
-    let mosi1   = p.PIN_11; // SPI1 TX
+    let mosi1 = p.PIN_11; // SPI1 TX
     let sck1 = p.PIN_10; // SPI1 SCK
     let cs1 = p.PIN_9; // SPI1 CSN
-    let dcx1 = p.PIN_14; // SPI1 RX
 
     
-    // create SPI
     let mut display_config = spi::Config::default();
     display_config.frequency = DISPLAY_FREQ;
     display_config.phase = spi::Phase::CaptureOnSecondTransition;
     display_config.polarity = spi::Polarity::IdleHigh;
 
-    let spi = Spi::new(p.SPI1, sck1, mosi1, miso1, p.DMA_CH0, p.DMA_CH1, display_config.clone());
 
-    // Create shared SPI bus
-    static SPI_BUS: StaticCell<Mutex<NoopRawMutex, Spi<'static, embassy_rp::peripherals::SPI1, Async>>> = StaticCell::new();
-    let spi_bus = SPI_BUS.init(Mutex::new(spi));
-    let spi_device = SpiDevice::new(spi_bus, Output::new(cs1, Level::High));
+    // create SPI0
+    let spi0: Spi<'_, embassy_rp::peripherals::SPI0, Async> = 
+        Spi::new(p.SPI0, sck0, mosi0, miso0, p.DMA_CH0, p.DMA_CH1, display_config.clone());
+    static SPI0_BUS: StaticCell<Mutex<NoopRawMutex, Spi<'static, embassy_rp::peripherals::SPI0, Async>>> = StaticCell::new();
+    let spi0_bus = SPI0_BUS.init(Mutex::new(spi0));
+    let spi0_device = SpiDevice::new(spi0_bus, Output::new(cs0, Level::High));
 
     // dcx: 0 = command, 1 = data
-    let dcx = Output::new(dcx1, Level::Low);
-    let rst = Output::new(rst1, Level::Low);
+    let dcx0_out = Output::new(dcx0, Level::Low);
+    let rst0_out = Output::new(rst0, Level::Low);
 
     // LCD backlight -- initially off
-    let mut lcd_bl = Output::new(bl1, Level::Low);
+    let mut bl0_out = Output::new(bl0, Level::Low);
 
     // display interface abstraction from SPI and DC
-    let di = SpiInterface::new(spi_device, dcx);
+    let spi_int0 = SpiInterface::new(spi0_device, dcx0_out);
 
     // Define the display from the display interface and initialize it
-    let mut display = Builder::new(ST7789, di)
-        .reset_pin(rst)
+    let mut display0_base = Builder::new(ST7789, spi_int0)
+        .reset_pin(rst0_out)
         .display_size(240, 320)
         .orientation(Orientation::new().rotate(Rotation::Deg90))
         .invert_colors(ColorInversion::Inverted)
@@ -101,6 +104,38 @@ async fn main(_spawner: Spawner) {
         .await
         .unwrap();
 
+    let mut display0 = FlipX::new(display0_base);
+
+    // create SPI1
+    let spi1: Spi<'_, embassy_rp::peripherals::SPI1, Async> = 
+        Spi::new(p.SPI1, sck1, mosi1, miso1, p.DMA_CH2, p.DMA_CH3, display_config.clone());
+
+    // Create shared SPI1 bus
+    static SPI1_BUS: StaticCell<Mutex<NoopRawMutex, Spi<'static, embassy_rp::peripherals::SPI1, Async>>> = StaticCell::new();
+    let spi1_bus = SPI1_BUS.init(Mutex::new(spi1));
+    let spi1_device = SpiDevice::new(spi1_bus, Output::new(cs1, Level::High));
+
+    // dcx: 0 = command, 1 = data
+    let dcx1_out = Output::new(dcx1, Level::Low);
+    let rst1_out = Output::new(rst1, Level::Low);
+
+    // LCD backlight -- initially off
+    let mut bl1_out = Output::new(bl1, Level::Low);
+
+    // display interface abstraction from SPI and DC
+    let spi_int1 = SpiInterface::new(spi1_device, dcx1_out);
+
+    // Define the display from the display interface and initialize it
+    let mut display1 = Builder::new(ST7789, spi_int1)
+        .reset_pin(rst1_out)
+        .display_size(240, 320)
+        .orientation(Orientation::new().rotate(Rotation::Deg90))
+        .invert_colors(ColorInversion::Inverted)
+        .init(&mut Delay)
+        .await
+        .unwrap();
+
+    
     // Initialize frame buffer
     let frame_buffer = FRAME_BUFFER.init([0; FRAME_SIZE_BYTES]);
 
@@ -136,7 +171,7 @@ async fn main(_spawner: Spawner) {
     info!("Config done");
 
     // Enable LCD backlight
-    lcd_bl.set_high();
+    bl1_out.set_high();
 
 
     loop {
@@ -158,12 +193,22 @@ async fn main(_spawner: Spawner) {
         }
 
         // Send the framebuffer data to the display
-        display
+
+        display0
+            .show_raw_data(0, 0, 
+                    DISPLAY_WIDTH.try_into().unwrap(), DISPLAY_HEIGHT.try_into().unwrap(), 
+                    frame_buffer)
+                .await
+                .unwrap();
+
+                display1
             .show_raw_data(0, 0, 
                 DISPLAY_WIDTH.try_into().unwrap(), DISPLAY_HEIGHT.try_into().unwrap(), 
                 frame_buffer)
             .await
             .unwrap();
+
+        
 
         led.set_low();
     }
